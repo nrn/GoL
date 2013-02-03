4368c1c755a5638f3af19ae060ef523d4997c331
Nick Niemeir <nick.niemeir@gmail.com>
2013-02-02 23:51:30 -0700

    Test stability, negative numbers

--- t/life.js
+++ t/life.js
@@ -12,11 +12,18 @@ test('Game of life', function (t) {
     , { x: 5, y: 5 }
     , { x: 6, y: 5 }
     ]
+  var block =
+    [ { x: -1, y: -1 }
+    , { x: -1, y: 0 }
+    , { x: 0, y: -1 }
+    , { x: 0, y: 0 }
+    ]
 
-  t.plan(5)
-  t.deepEqual(life(gen1), gen2, 'First update')
-  t.deepEqual(life(gen2), gen1, 'Second update')
+  t.same(life(gen1), gen2, 'First update')
+  t.same(life(gen2), gen1, 'Second update')
+  t.same(life(block), block, 'Stable')
   t.equal(life.findNeighbors({}, { x: 5, y: 5})['6,6'].value, 1, 'findNeighbors.')
   t.equal(life.add('a', 1, {}).value, 1, 'Add')
-  t.deepEqual(life.build({'1,1': { value: 3 }})[0], { x: 1, y: 1 }, 'Build')
+  t.same(life.build({'1,1': { value: 3 }})[0], { x: 1, y: 1 }, 'Build')
+  t.end()
 })
